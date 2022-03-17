92637695d7babcf0fa91393a7ec1269201dcdbb5
felipecazotti <felipecazotti@gmail.com>
2022-03-17 16:29:33 -0300

    Exercício 1 - Inverter Números

---
+++ Exercicio01/Inverter/Inverter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Inverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long[] numeros = {15485448, 2195498, 12, 98716549, 9999998999, 1654891};
+            string[] numerosTexto = new string[numeros.Length]; 
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                numerosTexto[i] = numeros[i].ToString();
+            }
+
+            for (int i = 0; i < numerosTexto.Length; i++)
+            {
+                Console.WriteLine($"{numerosTexto[i]} -> {InverterTexto(numerosTexto[i])}");
+            }
+        }
+
+        static string InverterTexto(string texto)
+        {
+            char[] textoChar = texto.ToCharArray();
+            Array.Reverse(textoChar);
+            return new string(textoChar);
+        }
+    }
+}
+
+
